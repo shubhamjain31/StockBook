ea1085aec9dce9be8ed14c514eb39ce57cfea877
shubhamjain31 <sj27754@gmail.com>
2021-12-18 12:06:35 +0530

    working on supplier

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+
+class Supplier(models.Model):
+    user  		 	= models.OneToOneField(User, on_delete=models.CASCADE)
+    name  		 	= models.CharField(max_length=120, unique=True)
+    address 	 	= models.CharField(max_length=220)
+    created_date 	= models.DateField(auto_now_add=True)
+    ip_address      = models.CharField(max_length=100, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,7 +1,55 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from StoreBook.decorators import get_ip
+
+from .models import (
+    Supplier,
+    # Buyer,
+    # Season,
+    # Drop,
+    # Product,
+    # Order,
+    # Delivery
+)
+from .forms import (
+    SupplierForm,
+    # BuyerForm,
+    # SeasonForm,
+    # DropForm,
+    # ProductForm,
+    # OrderForm,
+    # DeliveryForm
+)
 
 # Create your views here.
 
 
 def index(request):
 	return render(request, "index.html")
+
+@login_required(login_url='login')
+def suppliers(request):
+    forms = SupplierForm()
+
+    if request.method == 'POST':
+        forms = SupplierForm(request.POST)
+
+        if forms.is_valid():
+            name 			= forms.cleaned_data['name']
+            address 		= forms.cleaned_data['address']
+            email 			= forms.cleaned_data['email']
+            username 		= forms.cleaned_data['username']
+            password 		= forms.cleaned_data['password']
+            retype_password = forms.cleaned_data['retype_password']
+
+            if password == retype_password:
+                user = User.objects.create_user(
+                    username=username, password=password,
+                    email=email, is_supplier=True
+                )
+                Supplier.objects.create(user=user, name=name, address=address, ip_address=get_ip(request))
+                # return redirect('supplier-list')
+    
+    params = {'form': forms}
+    return render(request, 'store/supplier.html', params)
